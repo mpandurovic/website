f452618f5c1caec67621a3b763a4f19e2e058616
Marko Pandurovic <marko.istimd@gmail.com>
2018-09-22 11:54:00 +0200

    Fix FOUC in prod by adding mini css extract plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './css/styles.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+AOS.init({
+    duration: 1000,
+    once: true,
+    delay: 100,
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,26 @@
 const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
     mode: 'production',
     devtool: 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+            chunkFilename: "[id].[hash].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    }
 });
